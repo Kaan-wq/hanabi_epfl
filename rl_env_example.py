9a3e0e16a2e292f232794b8c2fce51876996cf84
Kaan-wq <kaanucar9@gmail.com>
2024-12-18 14:28:34 +0100

    Update rl_env_example.py

--- rl_env_example.py
+++ rl_env_example.py
@@ -77,7 +77,7 @@ class Runner(object):
                 file_path="experiments/policies/alphazero.jsonl",
             )
             (self.network, self.optimizer, self.criterion_value, self.num_actions) = (
-                initialize_training_components(self.environment, self.device, lr=1e-4, from_pretrained="saved_models/alphazero_model_100.pth")
+                initialize_training_components(self.environment, self.device, lr=1e-5, from_pretrained="saved_models/alphazero_model_100.pth")
             )
             # ,from_pretrained="saved_models/policy_model_200.pth"
 
